21f0ad949ad0c9af4b2af993afe49ac46626b1d2
Jairo Tuboi <tuboi.jairo@gmail.com>
2014-08-02 02:22:32 -0300

    arrumado o menu categorias e adicionado lista de sites de cada categoria.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,7 @@
 class MainController < ApplicationController
   def index
+    @menu = JSON.parse( '{"categories": [ {"name": "all", "sites": ["-"]},   {"name": "animes", "sites": ["site 1", "site 2"]},  {"name": "jogos","sites": ["site 3"]} ,  {"name": "blogs","sites": ["site 5","site 6","site 7"]} ]}');
   end
+  
+  
 end
